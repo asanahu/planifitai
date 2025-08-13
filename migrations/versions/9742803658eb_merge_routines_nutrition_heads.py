14a6563bd4b8e961c68ff73a93770fa110d743df
Alberto <albertosanahuja@gmail.com>
2025-08-13 18:28:03 +0200

    cambios

---
+++ migrations/versions/9742803658eb_merge_routines_nutrition_heads.py
@@ -0,0 +1,28 @@
+"""merge routines+nutrition heads
+
+Revision ID: 9742803658eb
+Revises: 123456789abc, d1a5f8bda1a6
+Create Date: 2025-08-13 16:24:50.729620
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9742803658eb'
+down_revision: Union[str, Sequence[str], None] = ('123456789abc', 'd1a5f8bda1a6')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
